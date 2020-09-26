4e9cb996ac161ef134a8d4d2509e90ef5d98fd2b
Nikita <nikitakalmycin@gmail.com>
2020-09-26 16:08:49 +0300

    First commit

---
+++ vk_dark_mode.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @run-at       document-start
+// @name         VKDark
+// @version      0.0.1
+// @description  Adds a nice, black-whity night mode for vk.
+// @author       https://vk.com/0xc0a80001
+// @match        *://*.vk.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
